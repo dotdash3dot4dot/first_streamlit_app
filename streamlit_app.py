50f0c980e433fa55ad5cde5249288d53b530658d
dotdash3dot4dot <46552708+dotdash3dot4dot@users.noreply.github.com>
2023-10-30 17:50:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
    
 streamlit.title("My first Streamlit Diner App")
 
-streamlit.header("Breakfast Menu")
+streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free Range Egg')
